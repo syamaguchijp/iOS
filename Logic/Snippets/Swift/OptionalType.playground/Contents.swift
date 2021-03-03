4e75abe7e836ff92a17ccea80885c6d51902b4b0
syamaguchijp <bergjapan@gmail.com>
2021-03-03 11:18:41 +0900

    mod Snippet

--- Logic/Snippets/Swift/OptionalType.playground/Contents.swift
+++ Logic/Snippets/Swift/OptionalType.playground/Contents.swift
@@ -25,11 +25,51 @@ class Sample {
             return
         }
         print("after guard, fuga=\(fuga.lowercased())")
-        
+    }
+    
+    func methodB(person: Person?) {
+        if let p = person { // 参照を渡しているため、p.nameを変えると呼び出し元も変わる
+            p.name = "Yamaha"
+            print("p.name=\(p.name)")
+        }
+    }
+    
+    func methodC(person: Person?) {
+        if var p = person {
+            p = Person(name: "Kawasaki") // pには他のオブジェクトを参照させるため、呼び出し元には影響しない
+            print("p.name=\(p.name)")
+        }
+    }
+    
+    func methodD(person: Person?) {
+        guard var p = person else { // 参照を渡しているため、p.nameを変えると呼び出し元も変わる
+            return
+        }
+        p.name = "Suzuki"
+        print("p.name=\(p.name)")
+    }
+    
+    func methodE(person: Person?) {
+        guard var p = person else {
+            return
+        }
+        p = Person(name: "Kawasaki") // pには他のオブジェクトを参照させるため、呼び出し元には影響しない
+        print("p.name=\(p.name)")
+    }
+}
+
+class Person {
+    var name: String = ""
+    init(name: String) {
+        self.name = name
     }
 }
 
 Sample().methodA(h: nil)
 print("------------")
 Sample().methodA(h: "honda")
+print("------------")
+var person = Person(name: "Honda")
+Sample().methodE(person: person)
+print("person.name=\(person.name)")
 
